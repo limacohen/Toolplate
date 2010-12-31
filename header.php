71357603cd72c4a7bc0cc8e683198e559397201f
limacohen <david@limacohen.com>
2010-12-31 03:09:23 -0300

    added CSS reset

--- header.php
+++ header.php
@@ -3,18 +3,24 @@
  * @package WordPress
  * @subpackage Toolbox
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+?><!doctype html>
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title(''); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title><?php wp_title(''); ?></title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/reset.css">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">	
+	<?php wp_head(); ?>
+	<script src="<?php bloginfo( 'template_directory' ); ?>/js/libs/modernizr-1.6.min.js"></script>
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/libs/modernizr-1.6.min.js"></script>
-
-<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
